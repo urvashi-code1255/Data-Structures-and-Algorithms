72fe8f45ea640b8c51413e639da2598342ee6166
urvashi-code1255 <urvashianand0906@gmail.com>
2020-10-12 20:57:58 +0530

    name of the queue changed to q

--- DS implementations/QUEUES/Queue_Implementation.cpp
+++ DS implementations/QUEUES/Queue_Implementation.cpp
@@ -1,9 +1,10 @@
-// C++ program for array implementation of queue
+// C++ program for array implementation of q
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-// A class to represent a queue
+// A class to represent a q
 class Queue
 {
 public:
@@ -15,94 +16,94 @@ public:
 // function to create a queue of given capacity, it initializes size of queue as 0
 Queue *createQueue(unsigned capacity)
 {
-    Queue *queue = new Queue();
-    queue->capacity = capacity;
-    queue->front = queue->size = 0;
+    Queue *q = new Queue();
+    q->capacity = capacity;
+    q->front = q->size = 0;
 
-    // assigning last index of the queue to the rear.
-    queue->rear = capacity - 1;
+    // assigning last index of the q to the rear.
+    q->rear = capacity - 1;
     // dynamically allocating int array of capacity.
-    queue->array = new int[(queue->capacity * sizeof(int))]; 
-    return queue;
+    q->array = new int[q->capacity];
+    return q;
 }
 
 // Queue is full when size becomes equal to the capacity
-int isFull(Queue *queue)
+int isFull(Queue *q)
 {
-    return (queue->size == queue->capacity);
+    return (q->size == q->capacity);
 }
 
 // Queue is empty when size is 0
-int isEmpty(Queue *queue)
+int isEmpty(Queue *q)
 {
-    return (queue->size == 0);
+    return (q->size == 0);
 }
 
 // Function to add an item to the queue. It changes rear and size
-void enqueue(Queue *queue, int item)
+void enqueue(Queue *q, int item)
 {
     // if queue is full then we can't push more.
-    if (isFull(queue))
+    if (isFull(q))
         return;
     // updating rear to next index.
-    queue->rear = (queue->rear + 1) % queue->capacity;
+    q->rear = (q->rear + 1) % q->capacity;
     // storing the item at the rear index of the queue.
-    queue->array[queue->rear] = item;
+    q->array[q->rear] = item;
     // as we added one more element so increment the size of the queue.
-    queue->size = queue->size + 1;
-    cout << item << " enqueued to queue\n";
+    q->size = q->size + 1;
+    cout << item << " enqueued to q\n";
 }
 
 // Function to remove an item from queue. It changes front and size
-int dequeue(Queue *queue)
+int dequeue(Queue *q)
 {
     // if queue is empty then we can't dequeue more, so for handling the underflow we will return INT_MIN.
-    if (isEmpty(queue))
+    if (isEmpty(q))
         return INT_MIN;
     // as we dequeue from the front of the queue so we will store the front element in item variable.
-    int item = queue->array[queue->front];
+    int item = q->array[q->front];
     // updating the front to the next index.
-    queue->front = (queue->front + 1) % queue->capacity;
+    q->front = (q->front + 1) % q->capacity;
     // as we have removed one element so we will decrement the size of the queue by 1.
-    queue->size = queue->size - 1;
+    q->size = q->size - 1;
     return item;
 }
 
 // Function to get front of queue
-int front(Queue *queue)
+int front(Queue *q)
 {
     // in underflow condition we will return INT_MIN.
-    if (isEmpty(queue))
+    if (isEmpty(q))
         return INT_MIN;
     // otherwise returning the element at the front index of the queue.
-    return queue->array[queue->front];
+    return q->array[q->front];
 }
 
 // Function to get rear of queue
-int rear(Queue *queue)
+int rear(Queue *q)
 {
     // in underflow condition we will return INT_MIN.
-    if (isEmpty(queue))
+    if (isEmpty(q))
         return INT_MIN;
     // otherwise returning the element at the rear index of the queue.
-    return queue->array[queue->rear];
+    return q->array[q->rear];
 }
 
 // Driver code
 int main()
 {
     // creating a queue of maximum capacity 1000.
-    Queue *queue = createQueue(1000);
+    Queue *q = createQueue(1000);
 
-    enqueue(queue, 10);
-    enqueue(queue, 20);
-    enqueue(queue, 30);
-    enqueue(queue, 40);
+    enqueue(q, 10);
+    enqueue(q, 20);
+    enqueue(q, 30);
+    enqueue(q, 40);
 
-    cout << dequeue(queue) << " dequeued from queue\n";
+    cout << dequeue(q) << " dequeued from queue\n";
 
-    cout << "Front item is "<< front(queue) << endl;
-    cout << "Rear item is " << rear(queue) << endl;
+    cout << "Front item is " << front(q) << endl;
+    cout << "Rear item is " << rear(q) << endl;
 
     return 0;
 }
